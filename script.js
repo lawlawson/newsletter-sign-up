9d88f174e62084afe9996c288f27f4a5b4b7507d
Lawrence Narh-Lawson <lawrencen@hotmail.com>
2025-04-19 22:59:43 +0100

    basic script started

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log('Hello there');
+const emailErrorMsg = document.getElementById('email-error-msg');
+const emailErrorBackground = document.getElementById('email');
+
+emailErrorMsg.style.display = 'none';
+emailErrorBackground.style.backgroundColor = 'transparent';
+emailErrorBackground.style.border = '1px solid hsl(231, 7%, 60%)';
+
